7fe1264e338d0bdefc6b000619406fccba1982cc
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-06 19:18:17 -0700

    Create config.lua

---
+++ lua/voy_missionstatus_config/config.lua
@@ -0,0 +1,13 @@
+VOY_Mission_Status_Config = {}
+
+VOY_Mission_Status_Config_Version = 1
+
+VOY_Mission_Status_Config_Usergroups = {
+	["superadmin"] = true,
+}
+
+VOY_Mission_Status_Config_Command = "!mission"
+
+VOY_Mission_Status_Config_ScrW = 0 -- Move this in small numbers such as -0.05 or 0.05 then restart if you have to to see how it moves, negative goes left positive goes right.
+
+VOY_Mission_Status_Config_ScrH = -0.03 -- Same as the above, positive goes down negative goes up.
